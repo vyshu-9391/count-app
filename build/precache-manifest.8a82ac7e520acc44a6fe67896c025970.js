8c1434b06098e299e02977f7ff17903a2f18aa4e
vyshu-9391 <vyshudarsi@gmail.com>
2024-07-17 12:52:25 +0000

    Automated build files update

---
+++ build/precache-manifest.8a82ac7e520acc44a6fe67896c025970.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5e31daf915d4123a997",
+    "url": "/static/css/main.0ad997f3.chunk.css"
+  },
+  {
+    "revision": "a5e31daf915d4123a997",
+    "url": "/static/js/main.18040af9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bc99f7452e4c8a2c4793",
+    "url": "/static/css/2.f73f7e00.chunk.css"
+  },
+  {
+    "revision": "bc99f7452e4c8a2c4793",
+    "url": "/static/js/2.03ded776.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "389c2a63d92d53e51ca947d9eccb9244",
+    "url": "/index.html"
+  }
+];
